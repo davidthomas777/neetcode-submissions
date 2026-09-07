6a7a084e37981f3f36b37647859afc74aea66b51
David Thomas <71041663+davidthomas777@users.noreply.github.com>
2026-09-06 19:51:08 -0700

    Add: first-missing-positive - submission-4

---
+++ Data Structures & Algorithms/first-missing-positive/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            // while nums[i] is positive and equal to the size of 
+            // the array or less, and the current number in nums 
+            // doesn't already hold this value
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i + 1 != nums[i]) return i + 1;
+        }
+        return n + 1;
+    }
+};
